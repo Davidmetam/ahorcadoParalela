b5986d51212ae4b5bbfe4a4f142c61f45c8052c7
davidmetam <david.mendoza.tam@gmail.com>
2025-05-06 23:49:29 -0600

    modify server

--- src/main/java/org/example/servidor/ServidorAhorcado.java
+++ src/main/java/org/example/servidor/ServidorAhorcado.java
@@ -5,7 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServidorAhorcado {
-    public static final int PUERTO = 1234;
+    public static final int PUERTO = 5000;
 
     public static void main(String[] args) {
         System.out.println("Servidor del Ahorcado iniciado en el puerto " + PUERTO);
@@ -15,7 +15,6 @@ public class ServidorAhorcado {
                 Socket cliente = servidor.accept();
                 System.out.println("Cliente conectado: " + cliente.getInetAddress());
 
-                // Se atiende al cliente en un hilo separado
                 new Thread(new ManejadorCliente(cliente)).start();
             }
         } catch (IOException e) {
